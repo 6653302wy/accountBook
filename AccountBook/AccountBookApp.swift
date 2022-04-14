4503c98d6853ea4fda317cebd7a2d4bb6cbe2b01
wanpingping <wanpingping@innotechx.com>
2022-04-14 10:04:53 +0800

    Initial Commit

---
+++ AccountBook/AccountBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountBookApp.swift
+//  AccountBook
+//
+//  Created by 万萍萍 on 2022/4/14.
+//
+
+import SwiftUI
+
+@main
+struct AccountBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
